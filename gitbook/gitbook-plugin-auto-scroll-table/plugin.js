9f77eee988dcc7929291d6185531effb8718e130
zcx <zcx980605@sohu.com>
2021-03-04 19:07:59 +0800

    Build for 20210304_2 Update

---
+++ gitbook/gitbook-plugin-auto-scroll-table/plugin.js
@@ -0,0 +1,12 @@
+require(["gitbook", "jQuery"], function(gitbook, $) {
+  gitbook.events.bind("page.change", function() {
+    [].slice.call(document.querySelectorAll('table')).forEach(function(el){
+        var wrapper = document.createElement('div');
+        wrapper.className = 'table-area';
+        el.parentNode.insertBefore(wrapper, el);
+        el.parentNode.removeChild(el);
+        wrapper.appendChild(el);
+    });
+  });
+
+});
